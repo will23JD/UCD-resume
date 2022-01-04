bfcef6f284c8744404bd90b69debec1ab5111459
will23JD <will_jd@outlook.com>
2022-01-04 12:38:23 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-rope-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -71,3 +73,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
