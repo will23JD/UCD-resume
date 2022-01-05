6854f1dfd8f9aed63f851ab98c00bdbe928b6bf8
will23JD <will_jd@outlook.com>
2022-01-05 15:04:51 +0000

    Implement emailjs intergration on contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -60,12 +60,15 @@ function fetchGitHubInformation(event) {
             var userData = response[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
-            $("gh-repo-data").html(repoInformationHTML(repoData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('x-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
